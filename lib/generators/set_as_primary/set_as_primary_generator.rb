f9e09b44886490387d8f037efc771d25c9db9fe7
Santosh Wadghule <santosh.wadghule@gmail.com>
2019-12-31 16:43:54 +0530

    Constraint.

--- lib/generators/set_as_primary/set_as_primary_generator.rb
+++ lib/generators/set_as_primary/set_as_primary_generator.rb
@@ -9,16 +9,41 @@ module SetAsPrimary
 
       source_root File.expand_path("templates", __dir__)
       argument :table_name, type: :string
+      argument :flag_name, type: :string 
+      argument :owner_key, type: :string, default: ''
 
       desc "Adds a boolean column 'primary' to the given table."
 
       def copy_migration
         migration_template "migration.rb", "db/migrate/add_primary_column_to_#{table_name}.rb",
-          migration_version: migration_version
+          migration_version: migration_version,
+          index_on: index_on
       end
 
       def migration_version
         "[#{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}]"
+      end
+
+      def index_on
+        if owner_key.present?
+          klass = table_name.classify.constantize
+          owner_association = klass.reflect_on_association(owner_key.to_sym)
+
+          if owner_association.nil?
+            raise ActiveRecord::AssociationNotFoundError.new(klass, owner_key)
+          end
+          
+          owner_id_key = "#{owner_key}_id"
+          
+          if owner_association.options[:polymorphic]
+            owner_type_key = "#{owner_key}_type"
+            "%i[#{owner_id_key}, #{owner_type_key}, #{flag_name}]"
+          else
+            "%i[#{owner_id_key}, #{flag_name}]"
+          end
+        else
+          ":#{flag_name}"
+        end
       end
 
       def self.next_migration_number(dirname)
--- lib/generators/set_as_primary/templates/migration.rb
+++ lib/generators/set_as_primary/templates/migration.rb
@@ -1,6 +1,7 @@
 class AddPrimaryColumnTo<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def change
-    add_column :<%= table_name.to_sym %>, :primary, :boolean, default: false, null: false
+    add_column :<%= table_name %>, :<%= flag_name %>, :boolean, default: false, null: false
+    add_index :<%= table_name %>, <%= index_on.inspect %>, unique: true, where: "(<%= table_name %>.<%= flag_name %> IS TRUE)"
   end
 end
 
