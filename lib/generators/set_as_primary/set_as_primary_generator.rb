de62c5f22c297dcdefad63e8bd790b1b64a71662
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-01-01 19:36:02 +0530

    Comment constraint line.

--- lib/generators/set_as_primary/set_as_primary_generator.rb
+++ lib/generators/set_as_primary/set_as_primary_generator.rb
@@ -6,13 +6,13 @@ module SetAsPrimary
   module Generators
     class SetAsPrimaryGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-
       source_root File.expand_path("templates", __dir__)
+
+      desc "Adds a user defined boolean column to the given table."
+
       argument :table_name, type: :string
       argument :flag_name, type: :string 
       argument :owner_key, type: :string, default: ''
-
-      desc "Adds a boolean column 'primary' to the given table."
 
       def copy_migration
         migration_template "migration.rb", "db/migrate/add_primary_column_to_#{table_name}.rb",
--- lib/generators/set_as_primary/templates/migration.rb
+++ lib/generators/set_as_primary/templates/migration.rb
@@ -1,7 +1,9 @@
 class AddPrimaryColumnTo<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def change
     add_column :<%= table_name %>, :<%= flag_name %>, :boolean, default: false, null: false
-    add_index :<%= table_name %>, <%= index_on.inspect %>, unique: true, where: "(<%= table_name %>.<%= flag_name %> IS TRUE)"
+
+    # NOTE: Please uncomment this line if you want only one 'true' (constraint) in the table.
+    # add_index :<%= table_name %>, <%= index_on.inspect %>, unique: true, where: "(<%= table_name %>.<%= flag_name %> IS TRUE)"
   end
 end
 
